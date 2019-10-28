715bdc1529ed6c6b4d6b23d8d34fb3c7b5c763dd
Даниил <daniil@MacBook-Air-Daniil.local>
2019-10-28 09:58:28 +0300

    Added functions

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,4 +1,17 @@
-class CinemaParser:
-    def __init__(self, city='msk'):
-        self.city = city
+'''core.py'''
 
+import requests
+
+class CinemaParser:
+    '''Parser of Cinema site'''
+    def __init__(self, city='msk'):
+        '''Defining city'''
+        self.city = city
+        self.url = 'https://{}.subscity.ru'.format(self.city)
+        self.content = None
+    def extract_raw_content(self):
+        '''Extract content'''
+        self.content = requests.get(self.url).text
+    def print_raw_content(self):
+        '''Print content'''
+        print(self.content)
